26695310e533dae318e19b6f1431a91c08c02d3c
kevinsun203 <kevingrandflyer@gmail.com>
2023-03-04 15:57:41 -0500

    Modified package.json and added aws dependancy.

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 const app = express();
 
 app.get('/', (req, res) => {
@@ -8,3 +11,9 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+s3.putObject({
+  Body: "helloWorld", 
+  Bucket: "techtogether", 
+  Key: "newfile.txt"
+}).promise();
